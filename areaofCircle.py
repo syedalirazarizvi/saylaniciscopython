a1528dea899fb0cb0585f0378b0cd8cc3db01d88
Syed Ali Raza <46275686+syedalirazarizvi@users.noreply.github.com>
2019-11-23 00:02:23 +0500

    Delete areaofCircle.py

--- areaofCircle.py
+++
@@ -1,8 +0,0 @@
-print("Hello and Welcome to Area of a Circle Calculator \n")
-
-cirRadius = int(input("Enter the Radius of Circle : "))
-
-calcRadius = (cirRadius**2)*3.14
-
-print("The area of the Circle as per your given Radius is : ", calcRadius)
-
