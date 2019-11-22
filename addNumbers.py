5ada90cf109b4f42dea8f03a612556dcaf256985
Syed Ali Raza <46275686+syedalirazarizvi@users.noreply.github.com>
2019-11-23 00:02:14 +0500

    Delete addNumbers.py

--- addNumbers.py
+++
@@ -1,9 +0,0 @@
-# Add Two Numbers
-
-firstNum = int(input("Enter First number : "))
-secondNum = int(input("Enter Second number : "))
-
-addResult = firstNum + secondNum
-
-print("The Total of both given values is : ", addResult)
-
